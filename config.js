56d2a672448d8f842685ed891f827d9931b16284
bbee84726-ux <bbee84726@gmail.com>
2026-08-28 00:45:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/bbee84726-ux/orbit/blob/main/images/orbit%20live.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*ORBIT Is Alive Now😍*",
+BOT_OWNER: '94740217262',  // Replace with the owner's phone number
 
 
 
