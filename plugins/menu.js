7b393b8162ecb34c9375f3aacb2a9139cfabf667
bbee84726-ux <bbee84726@gmail.com>
2026-08-28 00:49:49 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,7 +8,7 @@ cmd(
     filename: __filename,
   },
   async (
-    danuwa,
+    ORBIT,
     mek,
     m,
     {
@@ -29,7 +29,7 @@ cmd(
         });
       }
 
-      let menuText = "📋 *Available Commands:*\n";
+      let menuText = "📋 *ORBIT Commands:*\n";
 
       for (const [cat, cmds] of Object.entries(categories)) {
         menuText += `\n📂 *${cat.toUpperCase()}*\n`;
